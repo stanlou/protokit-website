e27fc65863da864bbc063e58a92e88be68fe7c4c
stanlou <ghassen.loukil.it@gmail.com>
2025-11-02 23:58:25 +0100

    fix assets

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,16 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
-  search: {
-    codeblocks: false,
-  },
+  search: { codeblocks: false },
 });
+
+const isProd = process.env.NODE_ENV === "production";
 
 export default withNextra({
   reactStrictMode: true,
+  output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: "/protokit-website", 
+  assetPrefix: "/protokit-website/" ,
 });
