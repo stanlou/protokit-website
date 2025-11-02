1835eb890cda46d40f49399af2106f2d5a7966e7
stanlou <ghassen.loukil.it@gmail.com>
2025-11-02 23:46:51 +0100

    use pages configure

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,14 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
-  search: { codeblocks: false },
+  search: {
+    codeblocks: false,
+  },
 });
-
-const isProd = process.env.NODE_ENV === "production";
 
 export default withNextra({
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isProd ? "/protokit-website" : "", 
-  assetPrefix: isProd ? "/protokit-website/" : "",
 });
