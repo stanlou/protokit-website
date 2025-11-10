0a0d7168a1a6e5346deb2df9ff2ab4777bfeb097
stanlou <ghassen.loukil.it@gmail.com>
2025-11-10 23:38:35 +0100

    use export instead

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 export default withNextra({
   reactStrictMode: true,
+  output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
   basePath: isProd ? basePath : "",
